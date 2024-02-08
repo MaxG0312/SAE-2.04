b58cc1dd062bfee5c1d0b288767ea77e090bca4b
cbahouas-iut90 <bahouassiladieuveille@gmail.com>
2024-02-09 00:49:26 +0100

    Remodication du fichier de connexion car l'utilisation de dotenv est obligatoire

--- connexion_db.py
+++ connexion_db.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 
 import pymysql.cursors
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        #
         db = g._database = pymysql.connect(
-            host="chezxage.mysql.pythonanywhere-services.com",
-            # host="serveurmysql",
-            user="chezxage",
-            password="BDD_meuble",
-            database="chezxage$default",
+            host=os.environ.get("HOST"),
+            user=os.environ.get("LOGIN"),
+            password=os.environ.get("PASSWD"),
+            database=os.environ.get("DATABASE"),
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
