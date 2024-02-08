f4b1a36bc8450ab9985c250aa7babd5520d2656f
cbahouas-iut90 <149400325+cbahouas-iut90@users.noreply.github.com>
2024-02-09 00:30:40 +0100

    Update connexion_db.py
    retablissement BDD avec Mysql et non dotenv pour garantir la synchronisation avec le serveur pythonanywhere

--- connexion_db.py
+++ connexion_db.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 
 import pymysql.cursors
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
+        #
         db = g._database = pymysql.connect(
-            host=os.environ.get("HOST"),
-            user=os.environ.get("LOGIN"),
-            password=os.environ.get("PASSWD"),
-            database=os.environ.get("DATABASE"),
+            host="chezxage.mysql.pythonanywhere-services.com",
+            # host="serveurmysql",
+            user="chezxage",
+            password="BDD_meuble",
+            database="chezxage$default",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
