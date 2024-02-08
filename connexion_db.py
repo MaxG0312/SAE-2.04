b25b58ac33db061f9e5a2a813db3a1c71720bec8
cbahouas-iut90 <bahouassiladieuveille@gmail.com>
2024-02-08 23:43:04 +0100

    Mise à jour du login de la BDD en utilisant dotenv pour la connexion à la BDD.

--- connexion_db.py
+++ connexion_db.py
@@ -1,25 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 
 import pymysql.cursors
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        #
-        db = g._database = pymysql.connect(
-            host="chezxage.mysql.pythonanywhere-services.com",
-            # host="serveurmysql",
-            user="chezxage",
-            password="BDD_meuble",
-            database="chezxage$default",
-            charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor
-        )
-    return db
-
-from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
-
-import pymysql.cursors
 import os
 from dotenv import load_dotenv
 
