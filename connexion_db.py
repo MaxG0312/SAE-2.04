706a4b6593a9880942dad29604ad4351ada2428a
cbahouas-iut90 <bahouassiladieuveille@gmail.com>
2024-02-10 00:55:59 +0100

    Le filtre est opérationnel.

--- connexion_db.py
+++ connexion_db.py
@@ -4,9 +4,9 @@ import pymysql.cursors
 import os
 from dotenv import load_dotenv
 
-# load_dotenv()
-project_folder = os.path.expanduser('/home/chezxage/mysite/SAE-2_04/')  # adjust as appropriate (avec le dossier où se trouve le fichier .env et app.py)
-load_dotenv(os.path.join(project_folder, '.env'))
+load_dotenv()
+# project_folder = os.path.expanduser('/home/chezxage/mysite/SAE-2_04/')  # adjust as appropriate (avec le dossier où se trouve le fichier .env et app.py)
+# load_dotenv(os.path.join(project_folder, '.env'))
 
 def get_db():
     db = getattr(g, '_database', None)
